414432a9962901d45e96003897264c1d9c32cf9d
okada-takumi <okada@itpm-gk.com>
2017-12-31 12:55:30 +0000

    update1231_3

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,7 +2,7 @@
 #class Users::RegistrationsController < Devise::RegistrationsControllerとすることで、Devise::RegistrationsControllerを継承しています。
 class Users::RegistrationsController < Devise::RegistrationsController
   #devise/registrations_controllerのbuild_resource(hash=nil)を上書き
-  def build_resource(hash=nil)
+   def build_resource(hash=nil)
     hash[:uid] = User.create_unique_string
     super
   end

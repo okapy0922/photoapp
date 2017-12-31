9fb327509785580df07312259a3e22e7a21353a4
okada-takumi <okada@itpm-gk.com>
2017-12-31 11:44:53 +0000

    update1231

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,16 +3,16 @@ class PhotosController < ApplicationController
 
   def index
     @photos = Photo.all
-  # render json: @photos @photos = Photo.allでデータを全取得後JSON形式で表示
+    # render json: @photos @photos = Photo.allでデータを全取得後JSON形式で表示
   end
 
   def new
-     if params[:back]
-       @photo = Photo.new(photos_params)
-     else
-       @photo = Photo.new
-     end
-   end
+    if params[:back]
+      @photo = Photo.new(photos_params)
+    else
+      @photo = Photo.new
+    end
+  end
 
   def create
     @photo = Photo.new(photo_params)
@@ -27,31 +27,31 @@ class PhotosController < ApplicationController
 
   def edit
     @photo = Photo.find(params[:id])
+  end
+
+  def update
+    @photo = Photo.find(params[:id])
+
+    if @photo.update(photo_params)
+      redirect_to photos_path, notice: "更新しました"
+    else
+      render action: 'edit'
     end
+  end
 
-    def update
-      @photo = Photo.find(params[:id])
+  def destroy
+    @photo = Photo.find(params[:id])
+    @photo.destroy
+    redirect_to photos_path, notice: "削除しました"
+  end
 
-      if @photo.update(photo_params)
-        redirect_to photos_path, notice: "更新しました"
-      else
-        render action: 'edit'
-      end
-    end
+  protected
 
-    def destroy
-      @photo = Photo.find(params[:id])
-      @photo.destroy
-      redirect_to photos_path, notice: "削除しました"
-    end
+  def set_photo
+    @photo = Photo.find(params[:id])
+  end
 
-    protected
-
-    def set_photo
-      @photo = Photo.find(params[:id])
-    end
-
-    def photo_params
-        params.require(:photo).permit(:content, :img, :img_cache, :remove_img)
-    end
+  def photo_params
+    params.require(:photo).permit(:content, :img, :img_cache, :remove_img)
+  end
 end

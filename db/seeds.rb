e25d7a58f1ab85460986044e8d8f24791337ab76
okada-takumi <okada@itpm-gk.com>
2018-03-17 05:34:41 +0000

    repair_icon

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,17 @@
-10.times do |n|
+10.times do |n| # 10人分の架空のアカウントデータ投入
   email = Faker::Internet.email
   password = "password"
   user = User.create!(email: email,
-               password: password,
-               password_confirmation: password,
-               uid: "1234567#{n}",
-               provider: "",
-               )
-   5.times do |n|
-     Photo.create!(
-                      img: (open("#{Rails.root}/app/assets/images/selfyogre.png")),
-                      content: "test",
-                      user_id: user.id,
-                      )
-   end
+    password: password,
+    password_confirmation: password,
+    uid: "1234567#{n}",
+    provider: "",
+  )
+  5.times do |n| # 5件分まとめて投稿
+    Photo.create!(
+      img: (open("#{Rails.root}/app/assets/images/selfyogre.png")),
+      content: "test",
+      user_id: user.id,
+    )
+  end
 end
